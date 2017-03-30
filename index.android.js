b40ef79fceeccd4228fdcdc9dedb2f7581ebce07
Nguyen Duc Huy <duc.huy.ctlk@gmail.com>
2017-03-30 21:48:51 +0700

    [f] Story 01 - Implement homepage UI with gothere.sg logo, 2 text inputs for entering source/dest and a submit button

--- index.android.js
+++ index.android.js
@@ -9,23 +9,57 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
+  TextInput,
+  Image,
+  Button,
   View
 } from 'react-native';
+
+const onSubmitButtonPress = () => {};
 
 export default class GothereSgReactNative extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <View style={styles.main}>
+          <Image 
+            style={styles.logo}
+            source={require('./sticker_lg.png')} />
+
+          <View style={styles.form}>
+            <View style={styles.inputContainer}>
+              <Text style={styles.inputLabel}>
+                From:
+              </Text>
+            </View>
+
+            <View style={styles.inputContainer}>
+              <TextInput
+                underlineColorAndroid={'transparent'}
+                style={styles.input} />
+            </View>
+
+            <View style={styles.inputContainer}>
+              <Text style={styles.inputLabel}>
+                To:
+              </Text>
+            </View>
+
+            <View style={styles.inputContainer}>
+              <TextInput
+                underlineColorAndroid={'transparent'}
+                style={styles.input} />
+            </View>
+
+            <View style={styles.submitButtonContainer}>
+              <Button 
+                style={styles.submit}
+                title="Go There"
+                onPress={onSubmitButtonPress}
+                color="#b8e356" />
+            </View>
+          </View>
+        </View>
       </View>
     );
   }
@@ -34,19 +68,62 @@ export default class GothereSgReactNative extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#9FD045',
+    height: 550,
+  },
+  main: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    width: 300,
+    padding: 15,
   },
-  welcome: {
+  form: {
+    alignSelf: 'stretch',
+    marginTop: 25,
+  },
+  logo: {
+    height: 70,
+    paddingTop: 20,
+    paddingLeft: 20,
+    paddingRight: 20,
+    resizeMode: 'contain',
+    backgroundColor: 'transparent',
+  },
+  inputLabel: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    marginTop: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  inputContainer: {
+    flexDirection: 'row',
+    alignSelf: 'stretch',
+  },
+  input: {
+    flex: 1,
+    paddingLeft: 12,
+    paddingRight: 35,
+    fontSize: 15,
+    marginTop: 10,
+    height: 40,
+    borderRadius: 4,
+    borderColor: 'rgb(115, 115, 115)',
+    borderWidth: 1,
+    backgroundColor: '#FFFFFF',
+  },
+  submitButtonContainer: {
+    flexDirection: 'row',
+    marginTop: 30,
+  },
+  submit: {
+    flex: 0.3,
+    color: '#ffffff',
+    fontSize: 15,
+    fontWeight: 'bold',
+    borderRadius: 4,
+    borderColor: '#677E40',
+    borderWidth: 1,
   },
 });
 
