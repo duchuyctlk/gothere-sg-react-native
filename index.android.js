f318401447a85cb33ab9a63cc5ff1d207e91b62a
Nguyen Duc Huy <duc.huy.ctlk@gmail.com>
2017-04-16 11:20:23 +0700

    Support going back on the WebView

--- index.android.js
+++ index.android.js
@@ -12,15 +12,46 @@ import {
   BackAndroid
 } from 'react-native';
 
+var WEBVIEW_REF = 'webview';
+
 export default class GothereSgReactNative extends Component {
+  componentDidMount() {
+    BackAndroid.addEventListener('hardwareBackPress', this.handleBack);
+  }
+
+  componentWillUnmount() {
+    BackAndroid.removeEventListener('hardwareBackPress', this.handleBack);
+  }
+
   render() {
     return (
       <WebView
+        ref={WEBVIEW_REF}
         source={{uri: 'https://gothere.sg/m'}}
         style={styles.webview}
+        onNavigationStateChange={this.onNavigationStateChange}
       />
     );
   }
+
+  onNavigationStateChange = (navState) => {
+    this.setState({
+      canGoBack: navState.canGoBack
+    });
+  };
+
+  goBack = () => {
+    this.refs[WEBVIEW_REF].goBack();
+  };
+
+  handleBack = (() => {
+    if (this.state && this.state.canGoBack) {
+      this.goBack();
+      return true; //avoid closing the app
+    }
+  
+    return false; //close the app
+  });
 }
 
 const styles = StyleSheet.create({
